c88b5839949fc6967196081691f84274ba652b70
Darryl Pogue <dvpdiner2@gmail.com>
2018-07-18 11:29:49 -0700

    misc: repo setup (#1)
    Fixes: https://npm.community/t/splitting-clis-stringify-package-into-its-own-module/654

---
+++ test/linefeed.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const stringifyPackage = require('../index')
+const test = require('tap').test
+
+const dummy = { name: 'dummy' }
+
+test('stringify-package', function (t) {
+  t.match(stringifyPackage(dummy), /\n$/m, 'with no params uses \\n')
+  t.match(stringifyPackage(dummy, 2, '\n'), /\n$/m, 'uses \\n')
+  t.match(stringifyPackage(dummy, 2, '\r\n'), /\r\n$/m, 'uses \\r\\n')
+
+  t.match(stringifyPackage(dummy), /^ {2}"name": "dummy"/m, 'with no params uses 2-space indent')
+  t.match(stringifyPackage(dummy, 2, '\n'), /^ {2}"name": "dummy"/m, 'uses 2-space indent')
+  t.match(stringifyPackage(dummy, 4, '\n'), /^ {4}"name": "dummy"/m, 'uses 4-space indent')
+
+  t.end()
+})

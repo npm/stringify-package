f6172e5c1889611db9f3d4a553452ca457f1d669
Nick Reiley <bloomber111@gmail.com>
2019-09-12 09:23:25 +0500

    fix: strict comparison

---
+++ test/nonformatted.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const stringifyPackage = require('../index')
+const test = require('tap').test
+
+const dummy = { name: 'dummy' }
+
+test('stringify-package', function (t) {
+  t.equal(stringifyPackage(dummy, 0).split(/\r\n|\r|\n/).length, 2, '0 works')
+  t.end()
+})
